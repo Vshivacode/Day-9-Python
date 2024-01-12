bee05702c3ce65546007958d7641ed9cecbd418d
Vatnala Shiva Prasad <115530603+Vshivacode@users.noreply.github.com>
2024-01-12 21:05:36 +0530

    Secret-Auction.py

---
+++ 1. Secret-Auction.py
@@ -0,0 +1,52 @@
+from replit import clear
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Angela": 123, "James": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
+  
+
+# Output:
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\
+                       .-------------.
+                      /_______________\
+
+What is your name?: abc
+What is your bid?: $23456
+Are there any other bidders? Type 'yes or 'no'.
+yes
+What is your name?: poiuyt
+What is your bid?: $892
+Are there any other bidders? Type 'yes or 'no'.
+no
+The winner is abc with a bid of $23456
